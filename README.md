7e6e453ed802675bbdfccb502e3ba3998db94162
Craig Ziesman <craig.ziesman@gmail.com>
2021-11-15 16:20:40 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# amq-activemq-client
---
+++ src/main/java/com/redhat/activemq/Consumer.java
@@ -0,0 +1,19 @@
+package com.redhat.activemq;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Consumer.class);
+
+    @JmsListener(destination = "${queue.name}")
+    public void processMsg(String message) {
+
+        LOG.info("============= Received: " + message);
+    }
+
+}
